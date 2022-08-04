d20d896052065ee9454c9d3e28a43ef0dc759534
Art Beckett <89126163+Bear1986@users.noreply.github.com>
2022-08-04 05:49:34 +0100

    Accordion project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  const [questions, setQuestions] = useState(data)
+  const [questions] = useState(data)
   return <main>
     <div className="container">
       <h3>questions and answers about login</h3>
